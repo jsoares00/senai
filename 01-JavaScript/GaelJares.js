0a147c8e1b68f365b75ee723329e1c15469b14bd
JOÁS SOARES DE ANDRADE <joas_andrade@estudante.sesisenai.org.br>
2026-04-23 21:25:11 -0300

    atualizaçao

---
+++ 01-JavaScript/GaelJares.js
@@ -0,0 +1,36 @@
+alert(" Ola Mundo ")
+
+let lucroPorCaminhao, lucroTotal, caminhões
+
+caminhões = Number(prompt("digite o numero de caminhões para calcular o lucro : "))
+lucroPorCaminhao = Number((90 * 50) -450)
+lucroTotal = Number(lucroPorCaminhao * caminhões)
+alert("o lucro total com a vendas dos jacaressao de : R$"+ lucroTotal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // inteiro caminhoes
+    // real lucroPorCaminhao, lucroTotal
+    // lucroPorCaminhao = (90*50) - 450
+    // escreva(lucroPorCaminhao)
+    // escreva("Quantos caminhões: ")
+    // leia(caminhoes)
+    // lucroTotal = lucroPorCaminhao * caminhoes
+    // escreva("Lucro da temporada: " + lucroTotal)
---
+++ 01-JavaScript/IA.js
@@ -0,0 +1,36 @@
+alert("Ola Mundo")
+
+let tamanhoDoPrompt, totalTokens, custoPorToken, totalReais
+
+tamanhoDoPrompt = Number(prompt("digite o tamanho de promt usado :"))
+totalTokens = Number(tamanhoDoPrompt + 5)
+custoPorToken =Number(prompt("qual valor do custo por token ?"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // inteiro tamanhoDoPrompt
+    // inteiro totalTokens
+    // real custoPorToken = 0.151, totalReais
+
+    // escreva("Tamanho do prompt: ")
+    // leia(tamanhoDoPrompt)
+  
+    // totalTokens = tamanhoDoPrompt + 5
+    // totalReais = totalTokens * custoPorToken
+  
+    // escreva("Total de tokens: " + totalTokens)
+    // escreva("\nValor: R$" + totalReais)
