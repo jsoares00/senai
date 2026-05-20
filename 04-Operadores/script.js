6c0d856c490708482ac22a2d42303383adcd0880
JOÁS SOARES DE ANDRADE <joas_andrade@estudante.sesisenai.org.br>
2026-05-19 21:50:18 -0300

    atualizacao

--- 04-Operadores/script.js
+++ 04-Operadores/script.js
@@ -1,3 +1,58 @@
+let valorDADO=0, totalDADO=0,D4=0, D6=0,D10=0,D12=0
+function resetar(){
+    valorDADO= valorDADO*0
+    totalDADO= totalDADO*0
+    rolagemD4 = 0 
+document.getElementById('TotalDADO').innerHTML='Total valor dos Dados :'+totalDADO
+document.getElementById('valorDado').innerHTML='Ultimo Dado :'+valorDADO
+
+document.getElementById('listaDADOS').innerHTML += ""
+}
+
+function rolagemD12(){
+
+    valorDADO =Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    totalDADO = totalDADO + valorDADO 
+    D12= D12+1
+document.getElementById('TotalDADO').innerHTML='Total valor dos Dados :'+totalDADO
+document.getElementById('valorDado').innerHTML='Ultimo Dado :'+valorDADO
+document.getElementById('d12').innerHTML='numero de vezes DADO 4('+D12 +")"
+document.getElementById('listaDADOS').innerHTML += + valorDADO + '<br>'
+}
+
+function rolagemD10(){
+valorDADO =Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    totalDADO = totalDADO + valorDADO 
+    D10= D10+1
+document.getElementById('TotalDADO').innerHTML='Total valor dos Dados :'+totalDADO
+document.getElementById('valorDado').innerHTML='Ultimo Dado :'+valorDADO
+document.getElementById('d10').innerHTML='numero de vezes DADO 4('+D10 +")"
+document.getElementById('listaDADOS').innerHTML += + valorDADO + '<br>'
+}
+function rolagemD6(){
+
+    valorDADO =Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    totalDADO = totalDADO + valorDADO 
+    D6= D6+1
+document.getElementById('TotalDADO').innerHTML='Total valor dos Dados :'+totalDADO
+document.getElementById('valorDado').innerHTML='Ultimo Dado :'+valorDADO
+document.getElementById('d6').innerHTML='numero de vezes DADO 4('+D6 +")"
+document.getElementById('listaDADOS').innerHTML += + valorDADO + '<br>'
+}
+
+function rolagemD4(){
+valorDADO =Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+totalDADO = totalDADO + valorDADO
+D4= D4+1
+
+document.getElementById('TotalDADO').innerHTML='Total valor dos Dados :'+totalDADO
+document.getElementById('valorDado').innerHTML='Ultimo Dado :'+valorDADO
+document.getElementById('d4').innerHTML='numero de vezes DADO 4('+D4 +")"
+document.getElementById('listaDADOS').innerHTML += + valorDADO + '<br>'
+}
+
+
+
 
 
 let total = 0
