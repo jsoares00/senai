d331f3f362fe0af8f55efcbedce7607301ee4490
JOÁS SOARES DE ANDRADE <joas_andrade@estudante.sesisenai.org.br>
2026-06-16 21:58:05 -0300

    atualizaçao

--- 06-repetiçoes/script.js
+++ 06-repetiçoes/script.js
@@ -1,8 +1,86 @@
-var contador=0
-while(contador < 10){
-    alert("contador"+ contador)
+// var contador=0
+// while(contador < 10){
+//     alert("contador"+ contador)
 
-    contador++
+//     contador++
+
+// }
+// console.log(contador)
+
+
+let numero=11
+function questao2(){
+if(numero <= 11 || numero > -1){
+
+    numero-=1
+
+document.getElementById("resultado2").innerHTML= " "+numero
 
 }
-console.log(contador)
+}
+
+function questao3(){
+
+}
+function x1(){
+    var tabuada=5
+tabuada*=1
+
+document.getElementById('resultado4').innerHTML="  "+tabuada
+}
+function x2(){
+    var tabuada=5
+
+tabuada*=2
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x3(){
+    var tabuada=5
+
+tabuada*=3
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x4(){
+var tabuada=5
+tabuada*=4
+
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x5(){
+    var tabuada=5
+tabuada*=5
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x6(){
+    var tabuada=5
+tabuada*=6
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x7(){
+    var tabuada=5
+tabuada*=7
+document.getElementById('resultado4').innerHTML=tabuada
+
+}
+function x8(){
+var tabuada=5
+tabuada*=8
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x9(){
+    var tabuada=5
+tabuada*=9
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function x10(){
+var tabuada=5
+tabuada*=10
+document.getElementById('resultado4').innerHTML=tabuada
+}
+function calcular(){
+    var tabuada=5,
+    multiplicdor=document.getElementById('multiplicar').value
+tabuada*=multiplicdor
+document.getElementById('resultado4').innerHTML=tabuada
+console.log(tabuada)
+}
